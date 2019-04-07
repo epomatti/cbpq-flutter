d07bf9d33fffe212b488820d018eb40f850375e6
Evandro Pomatti <evandro.pomatti@gmail.com>
2019-04-07 00:16:05 -0300

    error style

--- lib/screens/consulta_cadastro_form.dart
+++ lib/screens/consulta_cadastro_form.dart
@@ -165,6 +165,7 @@ class _ConsultaCadastroState extends State<ConsultaCadastroForm> {
           ),
         ),
         errorText: errorMessage != null ? errorMessage : null,
+        errorStyle: TextStyle(fontSize: 16),
       ),
     );
   }
