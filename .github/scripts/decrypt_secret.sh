ad63bf10ee5fb25bb538a6d7f7188d237dd8dd53
Evandro Pomatti <evandro.pomatti@gmail.com>
2019-10-26 16:26:31 -0300

    secrets

---
+++ .github/scripts/decrypt_secret.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Decrypt the file
+sudo mkdir $HOME/secrets
+# --batch to prevent interactive command --yes to assume "yes" for questions
+sudo gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
+--output $HOME/secrets/key.jks ./github/secrets/key.jks.gpg
+
+sudo mv $HOME/secrets/key.jks ./android/app/
